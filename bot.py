965010d3ef9522ae046e979b4d3709abc0e49993
unknown <alexboyuchen@gmail.com>
2020-11-14 00:39:33 -0800

    hiding token

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 import os
 from discord.ext import commands
 from confidentials import config
+from boto.s3.connection import S3Connection
+
+TOKEN = S3Connection(os.environ['TOKEN'])
 
 client = commands.Bot(command_prefix = 'boi ',help_command=None)
 
@@ -22,4 +25,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension('cogs.{0}'.format(filename[:-3]))
     
-client.run(config.token)
+client.run(TOKEN)
--- confidentials/config.py
+++ confidentials/config.py
@@ -1,2 +1,2 @@
 
-token = "Nzc1OTYzNzY1NjI3Mjg5NjMw.X6t-kg.LFSmzPUiUfMY-7H3Htq7rgkc9Nk"
+token = "no"
